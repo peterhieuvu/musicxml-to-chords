3684e4370d203e9b577dc018aa17944a5559bc2b
Peter Hieu Vu <peter.vu8@gmail.com>
2019-12-03 19:52:09 -0800

    add field to chord

--- chord.py
+++ chord.py
@@ -6,25 +6,27 @@ ROOT_BITS = 5 # 21
 BASS_BITS = 5 # 21
 KIND_BITS = 5 # 24
 
+NO_CHORD = -1 # Number representing no chord/end of piece
+
 # Parse and convert chord to integer representation
 def toInt(root, bass, kind):
     # First parse the chord
-    out: int = -1
+    out = -1
 
     # TODO: I first had the map not start at zero so that we could do some easier squashing
     # Down to the simplest representation, but I'm now thinking it's probably better to
     # just keep double sharps and flats since when they do appear, there's usually
     # a very good reason (functionally)
-    rooti = spelling.step2tpc(root) + spelling.TPC_MIN
-    bassi = spelling.step2tpc(bass) + spelling.TPC_MIN
+    rooti = spelling.step2tpc(root) - spelling.TPC_MIN
+    bassi = spelling.step2tpc(bass) - spelling.TPC_MIN
     kindi = spelling.kind2int(kind)
 
-    out += rooti
+    out = rooti
     out <<= BASS_BITS # make room for bass
     out += bassi
     out <<= KIND_BITS # make room for kind
     out += kindi
-    
+
     return out
 
 # Turn int representation back into root, bass, kind, and extension
